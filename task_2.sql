4eaf50f11789765d733b5d9f5b547349f53c8760
yoligAW27 <yohanagezahagn@gmail.com>
2025-12-13 02:04:30 -0800

    task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -39,10 +39,12 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
+
 
 
 
